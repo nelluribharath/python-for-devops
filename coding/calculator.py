ae5dba564c89925910b48632bf4228d522092083
BharathNellluri <82215464+nelluribharath@users.noreply.github.com>
2024-10-29 04:52:35 +0000

    calculator app

---
+++ coding/calculator.py
@@ -0,0 +1,33 @@
+#fuctions or definations
+
+a = 6
+b = 7
+
+#addition function
+
+def addition():
+    add = a+b
+    print(add)
+
+#sub
+
+def sub():
+    s = a-b
+    print(s)
+
+#multiplication
+
+def mul():
+    m = a*b
+    print(m)
+
+#division
+
+def div():
+    d = a/b
+    print(d)
+
+addition()
+sub()
+mul()
+div()
